ff9513395503d8a158fbe418d1092fcf6f2b9a39
Scott Freschet <scott.freschet@gmail.com>
2013-08-02 14:35:37 -0700

    Initial Commit

---
+++ iOS-SFSlideoutMenu/SFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFAppDelegate.h
+//  iOS-SFSlideoutMenu
+//
+//  Created by Scott Freschet on 8/2/13.
+//  Copyright (c) 2013 Scott Freschet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
